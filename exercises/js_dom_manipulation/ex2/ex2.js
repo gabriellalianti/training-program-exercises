67f56b36148642c68237c473de37d45152887a48
Gabriella L <134288268+gabriellalianti@users.noreply.github.com>
2024-09-26 11:08:16 +1000

    Complete exercise 2

--- exercises/js_dom_manipulation/ex2/ex2.js
+++ exercises/js_dom_manipulation/ex2/ex2.js
@@ -1,3 +1,13 @@
 console.log("Exercise 2")
 
-const main = document.querySelector("#display-box")
+// Alter .js and .html so that when button(x) is pressed, it will fit in the center box with the corresponding html
+
+
+// Bonus: make it so that the text fills in when the mouse leaves the button instead of the click event
+// Also add another button
+
+const box = document.querySelector("#display-box");
+
+function insertText(text) {
+    box.innerHTML = document.querySelector(text).innerHTML;
+}
