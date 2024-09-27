70b871fa33dc634450c8a01daac7543eaaf421bf
Gabriella L <134288268+gabriellalianti@users.noreply.github.com>
2024-09-27 20:14:17 +1000

    Complete exercise 3

--- exercises/js_dom_manipulation/ex3/ex3.js
+++ exercises/js_dom_manipulation/ex3/ex3.js
@@ -8,6 +8,11 @@ const main = document.querySelector("#display-box")
 const button = document.querySelector("button")
 
 button.addEventListener("click", () => {
-    fetch("://api.adviceslip.com/advice")
-        .then(...)
+    fetch("https://api.adviceslip.com/advice")
+    .then(resp => resp.json())
+    .then(data => {
+        console.log(data)
+        main.innerText = data.slip.advice
+    })
+    .catch(e => console.log(e))
 })
